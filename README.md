54a1199b2ef17436e905afe84a74e5f136e2c5a9
Vagacoder <qiruihu@gmail.com>
2019-07-30 11:12:45 -0700

    update with better design

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # Simple Popup Window using React
 07/30/2019
 ========================
-1. test the idea of popup window using react
-2. 
+1. Test the idea of popup window using react
+2. The mechanism:
+  2.1. the popup HTML-DOM is 100% wide and 100% high, so that it covers 
+      whole window, any other DOM elements are not interactable.
+  2.2. the elements inside popup DOM are aligned to center, the background
+      of popup DOM is black with the opacity of 0.5. So that other elements
+      are visible but dim.
 
 
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,21 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+class Popup extends Component {
+	render() {
+		return (
+			<div className='popup'>
+				<div className='popup_content'>
+					<h1>{this.props.infoPassed}</h1>
+					<button onClick={this.props.closePopup}>Close</button>
+				</div>
+			</div>
+		);
+	}
 }
+
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {showPopup: false};
+		this.togglePopup = this.togglePopup.bind(this);
+	}
+
+	togglePopup() {
+		this.setState({
+			showPopup: !this.state.showPopup
+		});
+	}
+	render() {
+		return (
+			<div className='app'>
+				<h1>Testing Popup Window</h1>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+					Ut lacinia leo sit amet purus consectetur, ac auctor eros condimentum. 
+					Nunc a tempor augue. Quisque arcu nisl, finibus at mollis vitae, 
+					efficitur at mi. Nullam at erat commodo neque dictum aliquet. 
+					Ut eget malesuada leo. Morbi dui velit, interdum eget scelerisque sit amet,
+					pellentesque a eros. Cras gravida interdum mattis. 
+					Duis condimentum gravida orci, nec tempor tellus lobortis vel. 
+					Fusce enim metus, iaculis non aliquam at, semper at lorem. 
+					Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere 
+					cubilia Curae; Aenean tempor odio nec scelerisque ultrices. 
+					Proin et erat orci. 
+				</p>
+				<button onClick={this.togglePopup}>Click to Show Popup</button>
+				{this.state.showPopup ?
+					<Popup
+						infoPassed='This is a Popup Window'
+						closePopup={this.togglePopup}
+					/> : null
+				}
+			</div>
+		);
+	}
+};
 
 export default App;
